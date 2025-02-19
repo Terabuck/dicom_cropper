6fdec57a12b3a11930d0a39b57f8af89e0f6d9a2
Ludwig Moreno <37218753+Terabuck@users.noreply.github.com>
2025-02-19 19:13:17 -0400

    v0.1
    Temporal update

--- dicom_cropper/settings_working01.py
+++ dicom_cropper/settings_working01.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Media configurations
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,26 +127,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'ERROR',  # Log only errors and above (INFO, DEBUG will be ignored)
-            'class': 'logging.StreamHandler',
-        },
-        'file': {
-            'level': 'DEBUG',  # You can keep DEBUG for more detailed logs
-            'class': 'logging.FileHandler',
-            'filename': 'django_debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
