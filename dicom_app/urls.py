870a7f82f4ae1701949c33644d642258b6d0a39c
Ludwig Moreno <37218753+Terabuck@users.noreply.github.com>
2025-02-19 20:27:46 -0400

    Update urls.py
    Clear ouputs path updated

--- dicom_app/urls.py
+++ dicom_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import upload_dicom, serve_dicom_file, crop_dicom  # Add crop_dicom
+from .views import upload_dicom, serve_dicom_file, crop_dicom, clear_outputs  # Add crop_dicom
 
 urlpatterns = [
     path('upload/', upload_dicom, name='upload_dicom'),
     path('dicom/<str:filename>/', serve_dicom_file, name='serve_dicom_file'),
     path('media/outputs/<str:filename>/', serve_dicom_file, name='serve_output_dicom'),
-    path('crop/', crop_dicom, name='crop_dicom'),  # Add this line
+    path('crop/', crop_dicom, name='crop_dicom'),
+    path('clear-outputs/', clear_outputs, name='clear_outputs'),
 ]
